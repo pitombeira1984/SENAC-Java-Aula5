2f8bc01fc6cef9ef08aaae8e48a73044868f345d
Aluno <aluno@alunos.ce.senac.br>
2026-03-30 21:00:38 -0300

    Aula 5

--- src/testandoAlerta/Tela.java
+++ src/testandoAlerta/Tela.java
@@ -1,10 +1,14 @@
 package testandoAlerta;
 
+import javax.swing.JOptionPane;
+
 public class Tela {
 
 	public static void main(String[] args) {
 		
-		Alerta alerta = new Alerta("Este é o meu Alerta");
+		String texto = JOptionPane.showInputDialog("Digite um Texto");
+		
+		Alerta alerta = new Alerta(texto);
 		
 		alerta.mostraAlerta();
 
